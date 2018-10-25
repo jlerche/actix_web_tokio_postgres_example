759d3e7926f681f70784117543af717a90fb7285
Jacob Lerche <jacob.lerche@gmail.com>
2018-10-24 23:19:15 -0700

    Gets a skeleton app up

--- src/database/db.rs
+++ src/database/db.rs
@@ -1,7 +1,34 @@
 use std::io;
 
-use tokio_postgres::{connect, Client};
+use actix;
+use actix::{WrapFuture, Actor, fut, ActorFuture, ContextFutureSpawner};
+use futures::{Future};
+use tokio_postgres;
 
 pub struct PgConnection {
-    client: Option<Client>
+    client: Option<tokio_postgres::Client>
+}
+
+impl actix::Actor for PgConnection {
+    type Context = actix::Context<Self>;
+}
+
+impl PgConnection {
+    pub fn connect(db_url: &str) -> actix::Addr<PgConnection> {
+        let hs = tokio_postgres::connect(db_url.parse().unwrap(), tokio_postgres::TlsMode::None);
+        PgConnection::create(move |ctx| {
+            let act = PgConnection {
+                client: None,
+            };
+
+            hs.map_err(|_| panic!("cannot connect to postgresql"))
+                .into_actor(&act)
+                .and_then(|(cl, conn), act, ctx| {
+                    actix::Arbiter::spawn(conn.map_err(|e| panic!("{}", e)));
+                    fut::ok(())
+                }).wait(ctx)
+            ;
+            act
+        })
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,33 @@ extern crate serde_derive;
 mod database;
 mod apps;
 
+use actix_web::{HttpRequest, HttpResponse, FutureResponse, http, AsyncResponder, middleware};
+
+use apps::app::AppState;
+use database::db::PgConnection;
+use futures::future;
+
 fn main() {
     ::std::env::set_var("RUST_LOG", "actix_web=info");
     env_logger::init();
 
+    let sys = actix::System::new("local_dev");
+    let db_url = "postgres://dev:dev@localhost/actix_web_tokio_postgres_example_development";
+
+    actix_web::server::new(move || {
+        let addr = PgConnection::connect(db_url);
+
+        actix_web::App::with_state(AppState{db: addr})
+            .middleware(middleware::Logger::default())
+            .resource("/healthz", |r| r.method(http::Method::GET).a(|_: &HttpRequest<AppState>| -> FutureResponse<HttpResponse> {
+                let fut = future::ok(HttpResponse::new(http::StatusCode::OK));
+                fut.responder()
+            }))
+    })
+        .bind("127.0.0.1:8080")
+        .unwrap()
+        .start();
+
+    println!("Started http server: 127.0.0.1:8080");
+    let _ = sys.run();
 }
